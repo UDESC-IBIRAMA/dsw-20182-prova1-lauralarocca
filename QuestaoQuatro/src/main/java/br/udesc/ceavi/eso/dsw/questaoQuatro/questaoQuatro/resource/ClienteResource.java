d73d9ab1cb2247bae853febb6001574c05db7910
lauralarocca <31144711+lauralarocca@users.noreply.github.com>
2018-10-08 21:51:19 -0300

    prova1 ultima versao

--- QuestaoQuatro/src/main/java/br/udesc/ceavi/eso/dsw/questaoQuatro/questaoQuatro/resource/ClienteResource.java
+++ QuestaoQuatro/src/main/java/br/udesc/ceavi/eso/dsw/questaoQuatro/questaoQuatro/resource/ClienteResource.java
@@ -5,10 +5,45 @@
  */
 package br.udesc.ceavi.eso.dsw.questaoQuatro.questaoQuatro.resource;
 
+import br.udesc.ceavi.eso.dsw.questaoQuatro.questaoQuatro.model.Cliente;
+import br.udesc.ceavi.eso.dsw.questaoQuatro.questaoQuatro.repository.ClienteRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
- * @author laura
+ * @author laura Questão 4
  */
+@RestController
+@RequestMapping("/cliente")
 public class ClienteResource {
+
+    @Autowired
+    private ClienteRepository cr;
+
+    //A e B(Persistido através do Spring)
+    @PostMapping
+    public void salvar(@RequestBody Cliente cliente) {
+        cr.save(cliente);
+    }
+
+    //C
+    @GetMapping
+    public List<Cliente> clientes() {
+        return cr.findAll();
+    }
+
+    //D
+    @PutMapping(consumes = "application/json")
+    public void update(@RequestParam Long id) {
+
+    }
 
 }
